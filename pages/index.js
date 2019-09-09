a738c7eb16a59a7500590976720f777f1928c23f
Andrew <olafblitz@gmail.com>
2019-09-08 23:16:54 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,13 +37,13 @@ export default () => (
           Our Congressmembers are supposed to work for us, the people. But right now, they’re only
           listening to their big donors. Use this tool to find out how to contact your Congressmembers and also view their major donors.
           <br />
-          <strong>It only takes a minute to make your voice heard today!</strong>
+          <strong>Make your voice heard today!</strong>
         </Heading.h2>
       </Container>
     </BackgroundImage>
     <Container width={1} maxWidth={36} py={[4, 5]} px={3}>
       <Heading.h2 fontSize={4} mb={2}>
-        Find out who represents you
+        Find out who represents you in Congress — it only takes five seconds
       </Heading.h2>
       <Search />
       <Card
@@ -54,7 +54,33 @@ export default () => (
         mt={[4, 5]}
       >
         <Heading.h2 fontSize={4} mb={2}>
-          Why should I call?
+          Why should I call? 
+        </Heading.h2>
+        <Text color="steel" fontSize={2}>
+          Calling your House Representative and State Senators is one of the
+          most effective ways to make your voice heard. Emails and letters are
+          often batched, and Tweets ignored, but speaking to a staffer on the
+          phone makes sure a real conversation happens with your
+          representatives. Check out Emily Ellsworth’s great{' '}
+          <A href="http://theslot.jezebel.com/how-to-effectively-lobby-your-congressperson-1788958124">
+            article
+          </A>
+          {' and '}
+          <A href="http://www.attn.com/stories/12768/former-congressional-staffer-explains-how-to-make-congressman-listen">
+            series of Tweets
+          </A>{' '}
+          to learn more.
+        </Text>
+      </Card>
+  <Card
+        p={[3, 4]}
+        mx={[-3, -4]}
+        bg="#fffae5"
+        boxShadowSize="md"
+        mt={[4, 5]}
+      >
+        <Heading.h2 fontSize={4} mb={2}>
+          Why should I call? 
         </Heading.h2>
         <Text color="steel" fontSize={2}>
           Calling your House Representative and State Senators is one of the

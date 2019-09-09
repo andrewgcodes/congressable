3ff6af2b4b910b72988f4181a8c3b5c436cca4df
Andrew <olafblitz@gmail.com>
2019-09-08 23:28:32 -0700

    Update Bio.js

--- components/Bio.js
+++ components/Bio.js
@@ -4,17 +4,16 @@ import { Flex, Avatar, Text, Link } from '@hackclub/design-system'
 const Bio = () => (
   <Flex align="center" my={2}>
     <Avatar
-      src="//lachlanjc.me/static/portrait-96.jpg"
-      alt="Lachlan Campbell"
+      src="https://media.licdn.com/dms/image/C5603AQEOCvLfx_LAzg/profile-displayphoto-shrink_200_200/0?e=1573689600&v=beta&t=txTihJR3jgGj98VnyRt4HHqnWUzNZoGQaR52JhTh2w8"
+      alt="Andrew Gao"
       size="48px"
       mr={3}
       style={{ flexShrink: '0' }}
     />
     <Text align="left" fontSize={2}>
-      A project by <strong>Lachlan Campbell</strong>, a web designer-developer
-      and high schooler.{' '}
-      <Link color="info" href="https://twitter.com/lachlanjc">
-        Follow them on Twitter
+      A project by <strong>Andrew Gao</strong>, forked from Lachlan Campbell .{' '}
+      <Link color="info" href="https://andrewgao.tech">
+        Visit Andrew's website.
       </Link>
     </Text>
   </Flex>
